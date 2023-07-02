6deaa42238f6f2f72840e4df920e9e2d02dbf24d
berna <adramalec@gmail.com>
2023-07-02 20:02:54 -0300

    Update main
    + embed
    + block reward
    + link block

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import asyncio
 import json
+
+import requests
 import websockets
 import discord
 from datetime import datetime
@@ -12,7 +14,6 @@ client = discord.Client(intents=intents)
 async def on_ready():
     print('Bot is ready!')
     channel = client.get_channel(CHANNEL_DISCORD)
-    await channel.send('Started reading websocket.')
     asyncio.ensure_future(read_websocket(channel, WALLET))
 
 async def read_websocket(channel, WALLET):
@@ -26,21 +27,50 @@ async def read_websocket(channel, WALLET):
                 await asyncio.sleep(60)
     while True:
         try:
-            async with websockets.connect('ws://derotest.friendspool.club:10301/info/',ping_interval=60, ping_timeout=120) as websocket:
+            async with websockets.connect('ws://derotest.friendspool.club:10301/info/', ping_interval=60, ping_timeout=120) as websocket:
                 print('Connected to websocket server.')
                 asyncio.ensure_future(send_ping())
                 while True:
                     try:
-                        result = json.loads(await websocket.recv())
-                        if "wallet" in result and result["wallet"] == WALLET:
+                        data = json.loads(await websocket.recv())
+                        if "wallet" in data and data["wallet"] == WALLET and data["event"] == "blockMined":
+                        #if "wallet" in data and data["wallet"] == 'dero1qyw28245wph625det80urwygufv23lhw8x8xajkawwdc5l0sl2l27qq2tsde9' and data["event"] == "blockMined":
+                            timestamp = datetime.fromtimestamp(data["timestamp"]/1000)
+                            timestamp = timestamp.strftime("%H:%M, %d/%m/%Y")
+                            height = data["height"]
+                            print(height)
+                            await asyncio.sleep(15)
+                            try:
+                                headers = {'Content-Type': 'application/json', }
+                                data = f'{{"jsonrpc": "2.0","id": "1","method": "DERO.GetBlock","params": {{"height": {height}}}}}'
+                                r = requests.post('http://derotest.friendspool.club:10102/json_rpc', headers=headers,
+                                              data=data).json()
+                                reward = r["result"]["block_header"]["reward"] / 1000000
+                            except:
+                                print("Não foi possível pegar o reward")
+                                reward = -1
+                            cotacao = requests.get("https://api.minerstat.com/v2/coins?list=" + "DERO").json()[0]["price"]
+                            #difficulty = data["difficulty"]
                             print("Miniblock!")
                             channel = client.get_channel(CHANNEL_DISCORD)
-                            embed = discord.Embed(title="Miniblock Found!", color=0xff0000)
+                            user = client.get_user(USER)  # eu
+                            embed = discord.Embed(title=f':partying_face: \t NOVO BLOCO SOLO!\n', colour=0x0000ff)
                             embed.set_thumbnail(url='https://docs.dero.io/assets/logo.png')
-                            embed.add_field(name="Height", value=result["height"], inline=True)
-                            embed.add_field(name="Timestamp", value=result["timestamp"], inline=True)
+                            total = reward * cotacao
+                            if reward != -1:
+                                embed.add_field(name=f':large_blue_diamond:  Valor:',
+                                                value=f'Block Reward: \t{reward:.6f} DERO'
+                                                      f'\nCotação:\t\t U${cotacao:.2f}'
+                                                      f'\n\nTotal: U${total:.2f}'
+                                                      f'\n'.replace(".", ","), inline=False)
+                            embed.add_field(name=f':stopwatch: Minerado:', value=f'às {timestamp}', inline=True)
+                            embed.add_field(name=":arrow_up: Altura do Bloco:", value=height, inline=True)
+                            embed.add_field(name="🌐  Link:", value=f'https://explorer.friendspool.club/block/{height}\n\u200b', inline=False)
+
+                            embed.set_footer(text="discord.io/BernaCripto")
                             try:
                                 await channel.send(embed=embed)
+                                await channel.send(f'{reward:.6f} DERO \t{user.mention}'.replace(".", ","))
                             except discord.errors.DiscordServerError as e:
                                 print(f"Error sending message: {e}")
                     except websockets.exceptions.ConnectionClosedError:
@@ -57,5 +87,8 @@ with open('secret.json') as e:
     CHANNEL_DISCORD = infos["discord"]
     WALLET = infos["wallet"]
     TOKEN_DISCORD = infos["token"]
+    USER = infos['user']
 
 client.run(TOKEN_DISCORD)
+
+
